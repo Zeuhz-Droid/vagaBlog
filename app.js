bed36729b1d9c88e2402d14f6313a65f71f014dc
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-10-29 23:44:52 +0100

    Installed app dependencies and initialized them

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const express = require("express");
-const blogRouter = require("./routes/blogRoutes");
-const globalErrorHandler = require("./controller/errorController");
+const express = require('express');
+const morgan = require('morgan');
+const passport = require('passport');
+
+const blogRouter = require('./routes/blogRoutes');
+const authRouter = require('./routes/userRoutes');
+
+const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
+require('dotenv').config();
 
-require("dotenv").config();
+require('./utilities/authenticate-passport-jwt');
 
-app.use(express.json({ extended: false }));
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(morgan('dev'));
+}
 
-app.use("/api/v1/blogs", blogRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use("*", (req, res, next) => {
-  next(err);
+app.use('/api/v1/blogs', blogRouter);
+app.use('/api/v1/users', authRouter);
+
+app.use('*', (req, res, next) => {
+  next(new Error(`Can't find ${req.originalUrl} on this server.`));
 });
 app.use(globalErrorHandler);
 

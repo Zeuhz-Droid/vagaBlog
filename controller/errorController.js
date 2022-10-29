75d47edd96fb0026823aeee5ea8bf0715d4ea6fe
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-10-29 23:45:09 +0100

    Global error middleware

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,8 +1,17 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+  err.status = err.status || 'error';
 
-  res.status(statusCode).json({
+  if ((process.env.NODE_ENV = 'development')) {
+    res.status(err.statusCode).json({
+      status: 'fail',
+      error: err,
+      message: err.msg,
+      stack: err.stack,
+    });
+  }
+
+  res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });

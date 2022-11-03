c0131d95997201d78acc372db9810b052c8f8458
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-11-03 09:25:16 +0100

    implemented an errorControl handler

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,18 +1,32 @@
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new Error(message);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: 'fail',
+    error: err,
+    message: err.msg,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if ((process.env.NODE_ENV = 'development')) {
-    res.status(err.statusCode).json({
-      status: 'fail',
-      error: err,
-      message: err.msg,
-      stack: err.stack,
-    });
+    sendErrorDev(err, res);
   }
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (err.name == 'CastError') handleCastErrorDB(err);
+  sendErrorProd(err, res);
 };

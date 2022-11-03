bed1a0f32b046d0c0440823abe5bd1a32a61d0a2
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-11-03 09:22:37 +0100

    Added a full name field to the user document

--- models/userModel.js
+++ models/userModel.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Last name is required'],
     trim: true,
   },
+  full_name: {
+    type: String,
+    trim: true,
+  },
   email: {
     type: String,
     required: [true, 'Please provide your email'],
@@ -29,12 +33,18 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ['user', 'admin'],
+    default: 'user',
   },
 });
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.pre('save', function (next) {
+  this.full_name = `${this.first_name} ${this.last_name}`;
   next();
 });
 
@@ -48,9 +58,3 @@ userSchema.methods.correctPassword = async function (
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
-// Users should have a first_name, last_name, email, password, (you can add other attributes you want to store about the user)
-// A user should be able to sign up and sign in into the blog app
-// Use JWT as authentication strategy and expire the token after 1 hour
-// A blog can be in two states; draft and published
-// Logged in and not logged in users should be able to get a list of published blogs created

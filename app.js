b466e3f1e3eab84ac155995a8b1b1685029ecbaa
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-11-04 14:48:26 +0100

    Modified the app module to accomodate the server

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
-const passport = require('passport');
 
 const blogRouter = require('./routes/blogRoutes');
-const authRouter = require('./routes/userRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const globalErrorHandler = require('./controller/errorController');
 
+require('./server')();
+
 const app = express();
+
 require('dotenv').config();
 
 require('./utilities/authenticate-passport-jwt');
@@ -20,11 +22,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1/blogs', blogRouter);
-app.use('/api/v1/users', authRouter);
+app.use('/api/v1/users', userRouter);
 
 app.use('*', (req, res, next) => {
   next(new Error(`Can't find ${req.originalUrl} on this server.`));
 });
 app.use(globalErrorHandler);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening succesfully on PORT: ${process.env.PORT}`)
+);
 
 module.exports = app;

9342d0acfb15448d113b1329c2e9b4384f3c2388
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-11-04 14:47:00 +0100

    Implemented get all users handler func

---
+++ controller/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utilities/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});

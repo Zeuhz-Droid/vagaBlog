b09399bebb4910621168c9fda64511828318e68a
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-02-06 13:21:10 +0100

    referenced rate-limter and logger in app.js file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const logger = require('./logger/logger');
 const httpLogger = require('./logger/httpLogger');
+const rateLimit = require('express-rate-limit');
 
 const blogRouter = require('./routes/blogRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -13,21 +14,26 @@ require('./database')();
 const app = express();
 
 require('dotenv').config();
-
-logger.info('does this work');
+require('./utilities/authenticate-passport-jwt');
 
 // this handles uncaught Exceptions in our codes like bad codes etc
 process.on('uncaughtException', (err) => {
-  console.log('UNHANDLED EXCEPTION! 💥 Shutting Down');
+  logger.error('UNHANDLED EXCEPTION! 💥 Shutting Down');
   process.exit(1);
 });
-
-require('./utilities/authenticate-passport-jwt');
 
 // this allows us get data on every request made to the database
 if ((process.env.NODE_ENV = 'development')) {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour.',
+});
+
+app.use(limiter);
 
 app.use(httpLogger);
 app.use(express.json());
@@ -43,12 +49,12 @@ app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
-  console.log(`Listening succesfully on PORT: ${process.env.PORT}`)
+  logger.info(`Listening succesfully on PORT: ${process.env.PORT}`)
 );
 
 // this handles uncaught rejections in our codes like un caught async functions
 process.on('unhandledRejection', (err) => {
-  console.log(`UNHANDLED REJECTION!  💥 Shutting Down (${err.statusCode})`);
+  logger.error(`UNHANDLED REJECTION!  💥 Shutting Down (${err.statusCode})`);
   server.close(() => {
     process.exit(1);
   });
